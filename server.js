46af2d7eeb8b710213329aa4ebaa7b3d316683d4
QTXAlgoSystems <qtxalgosystems@gmail.com>
2025-04-11 13:30:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000; // Default port 3000 if not provided
+const PORT = process.env.PORT || 3000; // Default port 3000 if not provided 
 const WEBHOOK_TOKEN = process.env.WEBHOOK_TOKEN;
 
 const ALLOWED_ORIGIN = "https://qtxalgosystems.com"; // Frontend domain
