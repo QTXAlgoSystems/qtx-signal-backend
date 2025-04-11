124f4be19eb346ab47c7363d0a4a57526decb407
QTXAlgoSystems <qtxalgosystems@gmail.com>
2025-04-11 02:41:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT; 
 const WEBHOOK_TOKEN = process.env.WEBHOOK_TOKEN;
 
 app.use(cors());
