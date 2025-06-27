d4e077b09d5a73ecdf13237053922c651578c906
QTXAlgoSystems <qtxalgosystems@gmail.com>
2025-06-27 02:42:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -565,9 +565,7 @@ app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
 
-const TelegramBot = require("node-telegram-bot-api");
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
 
 bot.onText(/\/start (.+)/, async (msg, match) => {
